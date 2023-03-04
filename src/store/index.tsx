dac5168831116441aecde451f356e66609f9dff7
Vladislav Iokhim <vladislaviokhim@Vladislavs-MacBook-Pro.local>
2023-03-04 18:45:06 +0200

    cleaning

---
+++ src/store/index.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+
+
+const rootReducer = {
+ 
+};
+
+export const store = configureStore({
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({ serializableCheck: false })
+});
+
+interface StoreProviderProps {
+  children: React.ReactNode;
+}
+
+
+
+const StoreProvider: React.FC<StoreProviderProps> = ({children}) => {
+  return <Provider store={store}>{children}</Provider>;
+};
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>;
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch;
+
+
+export default StoreProvider;
